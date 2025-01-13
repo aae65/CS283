1ec0e8fec061959700afd0d8382da2479915e3d9
Ashleigh Edwards <aae65@drexel.edu>
2025-01-13 18:59:26 -0500

    1/13/2025 - finished up, all comments added

--- Assignments/1-C-Refresher/stringfun.c
+++ Assignments/1-C-Refresher/stringfun.c
@@ -13,13 +13,15 @@ int  setup_buff(char *, char *, int);
 //prototypes for functions to handle required functionality
 int  count_words(char *, int, int);
 //add additional prototypes here
-void reverse_string(char *, char *, int);
+int reverse_string(char *, int);
+int word_print(char *, int);
+int search_and_replace(char *, char *, char *);
 
 
 int setup_buff(char *buff, char *user_str, int len){
     //#4:  Implement the setup buff as per the directions
     int str_length = 0;
-    for (int i = 0; i < *user_str; i++){ //while string is not equal to the null terminator
+    for (int i = 0; *(user_str + i) != '\0'; i++){ //while string is not equal to the null terminator
         if (*(user_str + i) != '\t' && *(user_str + i) != ' ' && *(user_str + i) != '\n'){ //if char is a non-white space character
             *(buff + str_length) = *(user_str + i); //then copy char to buffer
         } else {
@@ -32,13 +34,14 @@ int setup_buff(char *buff, char *user_str, int len){
     if (str_length > len){ //if string is greater than buffer size
         return -1; //then return string too large code
     }
+    len = str_length;
     
     while (str_length < BUFFER_SZ){ //while length of buffer is not equal to 50
         *(buff + str_length) = '.'; //add '.' to buffer
-        user_str++; 
+        str_length++;
     }
 
-    return str_length; //return size of user string 
+    return len; //return size of user string 
 }
 
 void print_buff(char *buff, int len){
@@ -55,29 +58,75 @@ void usage(char *exename){
 }
 
 int count_words(char *buff, int len, int str_len){
-    int wc = 0;
+    int wc = 0;         //initalize word count
 
-    for(int i = 0; i < str_len; i++){
-        if (buff[i] == ' ' && len > 0){
-            wc++;
+    if (str_len > len){         //if string length is greater than buffer length
+        return -2;                  //then return with -2
+    }
+
+    for(int i = 0; i < str_len; i++){                               //for loop
+        if (*(buff + i) == ' ' && i > 0 && *(buff + i - 1) != ' '){     //if buffer char is not space and i is not 0 and previous buffer char is not space
+            wc++;                                                           //then increase word count
         }
     }
 
-    return wc;
+    if (str_len > 0 && *(buff + str_len - 1) != ' '){               //if string length is greater than 0 and char is not space
+        wc++;                                                           //then increase word count
+    }
+
+    return wc;                                                      //return word count
 }
 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
-void reverse_string(char *str, char *buff, int str_len){
-    int end_idx = str_len - 1;
-    int start_idx = 0;
+int reverse_string(char *str, int str_len){
+    char temp;
 
-    while (end_idx > start_idx){
-        str[start_idx] = str[end_idx];
-        str[end_idx] = buff[start_idx];
-
-        start_idx++;
-        end_idx--;
+    for (int i = 0; i < str_len / 2; i++) {
+        temp = *(str + i);
+        *(str + i) = *(str + str_len - i - 1);
+        *(str + str_len - i - 1) = temp;
     }
+
+    return 0;
+
+}
+
+int word_print(char *str, int str_len){
+    int wlen = 0;                           //initilize word length
+    int wc = 0;                             //initalize word count
+
+    printf("Word Print\n----------\n");             //print beginning
+
+    while (*str != '\0'){                           //while string is not null terminated
+        while (*str == ' '){                            //while string is space
+            str++;                                          //move to next spot in string
+        }
+
+        if (*str != '\0'){                              //if string is not null terminated
+            wc++;                                           //then increase word count
+            printf("%d. ", wc);                             //print number
+
+            wlen = 0;                                       //set word length to 0
+            while (*str != ' ' && *str != '\0'){            //while string is not space and not null terminated
+                putchar(*str);                                  //put character to stdout
+                str++;                                          //move to next char
+                wlen++;                                         //increase word length
+            }
+
+            printf(" (%d)\n", wlen);                        //print # of chars
+        }
+        
+        if(wlen > str_len){                             //if word length is greater than string length
+            return -2;                                      //return with -2
+        }
+    }
+    return 0;                                       
+}
+
+int search_and_replace(char *user_str, char *search, char *replace){
+    printf("Replacing %c with %c in %c.\n", *user_str, *replace, *search);  //placeholder to prevent warning from compiler
+    printf("Not implemented!\n");                                           //
+    return -1;                                                              //return with -1
 }
 
 int main(int argc, char *argv[]){
@@ -88,7 +137,7 @@ int main(int argc, char *argv[]){
     int  rc;                //used for return codes
     int  user_str_len;      //length of user supplied string
 
-    //#1. WHY IS THIS SAFE, aka what if argv[1] does not exist?
+    //#1.   WHY IS THIS SAFE, aka what if argv[1] does not exist?
     //*     This is safe because it catches the input before running into trouble.
     //*     argv[0] is the filename (executable) that is being run and argv[1] is being
     //*     used as the parameter for the function. argc is the number of arguments, argv
@@ -109,7 +158,7 @@ int main(int argc, char *argv[]){
 
     //WE NOW WILL HANDLE THE REQUIRED OPERATIONS
 
-    //#2 Document the purpose of the if statement below
+    //#2    Document the purpose of the if statement below
     //*     The purpose of the if statement is to determine if the number of arguments
     //*     is less than 3. If it is, it prints the intended usage for the program
     //*     then exits with code 1. The reason this is here is to prevent errors from 
@@ -124,9 +173,9 @@ int main(int argc, char *argv[]){
     //#3. Allocate space for the buffer using malloc and
     //    handle error if malloc fails by exiting with a 
     //    return code of 99
-    *buff = (char *)malloc(BUFFER_SZ * sizeof(char));
+    buff = (char *)malloc(BUFFER_SZ * sizeof(char));
     if(buff == NULL){
-        fprintf(stderr, "Memory allocation failed");
+        fprintf(stderr, "Memory allocation failed\n");
         exit(99);
     }
 
@@ -141,18 +190,42 @@ int main(int argc, char *argv[]){
         case 'c':
             rc = count_words(buff, BUFFER_SZ, user_str_len);  //you need to implement
             if (rc < 0){
-                printf("Error counting words, rc = %d", rc);
+                printf("Error counting words, rc = %d\n", rc);
                 exit(2);
             }
             printf("Word Count: %d\n", rc);
             break;
 
-        //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
+        //#5 Implement the other cases for 'r' and 'w' by extending
         //       the case statement options
         case 'r':
-            reverse_string(input_string, buff, user_str_len);
+            rc = reverse_string(input_string, user_str_len);
+            if (rc < 0){
+                printf("Error reversing words, rc = %d\n", rc);
+                exit(2);
+            }
             printf("Reversed string: %s\n", input_string);
             break;
+
+        case 'w':
+            rc = word_print(input_string, user_str_len);
+            if (rc < 0){
+                printf("Error printing words, rc = %d\n", rc);
+                exit(2);
+            }
+            break;
+
+        case 'x':
+            if (argc != 5){
+                printf("Not enough arguments\n");
+                exit(2);
+            }
+            rc = search_and_replace(input_string, argv[2], argv[3]);
+            if (rc < 0){
+                exit(2);
+            }
+            break;
+
         default:
             usage(argv[0]);
             exit(1);
@@ -164,10 +237,17 @@ int main(int argc, char *argv[]){
     exit(0);
 }
 
-//TODO:  #7  Notice all of the helper functions provided in the 
+//    #7    Notice all of the helper functions provided in the 
 //          starter take both the buffer as well as the length.  Why
 //          do you think providing both the pointer and the length
 //          is a good practice, after all we know from main() that 
 //          the buff variable will have exactly 50 bytes?
 //  
-//          PLACE YOUR ANSWER HERE
+//*         It is important to include the length and the buffer  
+//*         because, when dealing with reallocation of memory,  
+//*         the length is not always going to remain the same.
+//*         For example, a string needing to be motified may
+//*         be larger than the provided allocation. So, the
+//*         length would have to be reallocated. Assuming the
+//*         string would be used for more than function, the 
+//*         length should always be provided.
